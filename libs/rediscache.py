61ab88901cd8b1006636a3c89fe94eb7f3f4c775
arieunier <arieunier@salesforce.com>
2019-01-31 15:24:51 +0100

    redis update

--- libs/rediscache.py
+++ libs/rediscache.py
@@ -3,7 +3,7 @@ import os
 import ujson
 from datetime import datetime 
 from libs import logs   
-
+import traceback
 REDIS_URL = os.getenv('REDIS_URL','')
 REDIS_CONN = None 
 
@@ -12,39 +12,75 @@ logger = logs.logger_init(loggername='app',
             debugvalue=logs.LOG_LEVEL,
             flaskapp=None)
 
+def __connect():
+    global REDIS_CONN
+    global REDIS_URL
+    if (REDIS_URL != ''):
+        REDIS_CONN = redis.from_url(REDIS_URL)
+        REDIS_CONN.set('key','value')
+        REDIS_CONN.expire('key', 300) # 5 minutes
+        logger.info("{}  - Initialization done Redis" .format(datetime.now()))
 
-if (REDIS_URL != ''):
-    REDIS_CONN = redis.from_url(REDIS_URL)
-    REDIS_CONN.set('key','value')
-    REDIS_CONN.expire('key', 300) # 5 minutes
-    logger.info("{}  - Initialization done Redis" .format(datetime.now()))
+
 
 def __display_RedisContent():
-    if (REDIS_CONN != None):
-        for keys in REDIS_CONN.scan_iter():
-            logger.info(keys)
-    cacheData = __getCache('keys *')
-    if cacheData != None:
-        for entry in cacheData:
-            logger.info(entry)
+    try:
+        global REDIS_CONN
+        if (REDIS_CONN == None):
+            __connect()
+        if (REDIS_CONN != None):
+            for keys in REDIS_CONN.scan_iter():
+                logger.info(keys)
+        cacheData = __getCache('keys *')
+        if cacheData != None:
+            for entry in cacheData:
+                logger.info(entry)
+    except Exception as e:
+        REDIS_CONN = None
+        traceback.print_exc()
 
 def __setCache(key, data, ttl):
-    key_str = ujson.dumps(key)
-    if (REDIS_CONN != None):
-        logger.debug('Storing in Redis')
-        REDIS_CONN.set(key_str, data)
-        REDIS_CONN.expire(key_str, ttl)
+    try:
+        global REDIS_CONN
+        if (REDIS_CONN == None):
+            __connect()
+
+        key_str = ujson.dumps(key)
+        if (REDIS_CONN != None):
+            logger.debug('Storing in Redis')
+            REDIS_CONN.set(key_str, data)
+            REDIS_CONN.expire(key_str, ttl)
+    except Exception as e:
+        REDIS_CONN = None
+        traceback.print_exc()
 
 def __getCache(key):
-    key_str = ujson.dumps(key)
-    if (REDIS_CONN != None):
-        logger.debug('Reading in Redis')
-        return REDIS_CONN.get(key_str)
-    return None 
+    try:
+        global REDIS_CONN
+        if (REDIS_CONN == None):
+            __connect()
+
+        key_str = ujson.dumps(key)
+        if (REDIS_CONN != None):
+            logger.debug('Reading in Redis')
+            return REDIS_CONN.get(key_str)
+        return None 
+    except Exception as e:
+        REDIS_CONN = None
+        traceback.print_exc()
+        return None
 
 def __delCache(key):
-    key_str = ujson.dumps(key)
-    if (REDIS_CONN != None):
-        logger.debug('Deleting in Redis')
-        REDIS_CONN.delete(key_str)
+    try:
+        global REDIS_CONN
+        if (REDIS_CONN == None):
+            __connect()
+
+        key_str = ujson.dumps(key)
+        if (REDIS_CONN != None):
+            logger.debug('Deleting in Redis')
+            REDIS_CONN.delete(key_str)
+    except Exception as e:
+        REDIS_CONN = None
+        traceback.print_exc()
     
--- setenv.sh
+++ setenv.sh
@@ -1,34 +1,36 @@
+APPNAME='infinite-escarpment-68092'
+
 export PYTHONPATH=.:./libs/:./appsrc/:./pyutils
-export BUCKETEER_AWS_ACCESS_KEY_ID=`heroku config:get BUCKETEER_AWS_ACCESS_KEY_ID --app yourdemo`
-export BUCKETEER_AWS_REGION=`heroku config:get BUCKETEER_AWS_REGION --app yourdemo`
-export BUCKETEER_AWS_SECRET_ACCESS_KEY=`heroku config:get BUCKETEER_AWS_SECRET_ACCESS_KEY --app yourdemo`
-export BUCKETEER_BUCKET_NAME=`heroku config:get BUCKETEER_BUCKET_NAME --app yourdemo`
-export CLOUDINARY_URL=`heroku config:get CLOUDINARY_URL --app yourdemo`
-export DATABASE_URL=`heroku config:get DATABASE_URL --app yourdemo`
-export LOGDNA_KEY=`heroku config:get LOGDNA_KEY --app yourdemo`
-export NEW_RELIC_APP_NAME=`heroku config:get NEW_RELIC_APP_NAME --app yourdemo`
-export NEW_RELIC_CONFIG_FILE=`heroku config:get NEW_RELIC_CONFIG_FILE --app yourdemo`
-export NEW_RELIC_LICENSE_KEY=`heroku config:get NEW_RELIC_LICENSE_KEY --app yourdemo`
-export NEW_RELIC_LOG=`heroku config:get NEW_RELIC_LOG --app yourdemo`
-export NEW_RELIC_LOG_LEVEL=`heroku config:get NEW_RELIC_LOG_LEVEL --app yourdemo`
-export REDIS_URL=`heroku config:get REDIS_URL --app yourdemo`
-export STAGING_DATABASE_URL=`heroku config:get STAGING_DATABASE_URL --app yourdemo`
-export CLOUDAMQP_APIKEY=`heroku config:get CLOUDAMQP_APIKEY --app yourdemo`
-export CLOUDAMQP_URL=`heroku config:get CLOUDAMQP_URL --app yourdemo`
-export PUSHER_SOCKET_URL=`heroku config:get PUSHER_SOCKET_URL --app yourdemo`
-export PUSHER_URL=`heroku config:get PUSHER_URL --app yourdemo`
+export BUCKETEER_AWS_ACCESS_KEY_ID=`heroku config:get BUCKETEER_AWS_ACCESS_KEY_ID --app $APPNAME`
+export BUCKETEER_AWS_REGION=`heroku config:get BUCKETEER_AWS_REGION --app $APPNAME`
+export BUCKETEER_AWS_SECRET_ACCESS_KEY=`heroku config:get BUCKETEER_AWS_SECRET_ACCESS_KEY --app $APPNAME`
+export BUCKETEER_BUCKET_NAME=`heroku config:get BUCKETEER_BUCKET_NAME --app $APPNAME`
+export CLOUDINARY_URL=`heroku config:get CLOUDINARY_URL --app $APPNAME`
+export DATABASE_URL=`heroku config:get DATABASE_URL --app $APPNAME`
+export LOGDNA_KEY=`heroku config:get LOGDNA_KEY --app $APPNAME`
+export NEW_RELIC_APP_NAME=`heroku config:get NEW_RELIC_APP_NAME --app $APPNAME`
+export NEW_RELIC_CONFIG_FILE=`heroku config:get NEW_RELIC_CONFIG_FILE --app $APPNAME`
+export NEW_RELIC_LICENSE_KEY=`heroku config:get NEW_RELIC_LICENSE_KEY --app $APPNAME`
+export NEW_RELIC_LOG=`heroku config:get NEW_RELIC_LOG --app $APPNAME`
+export NEW_RELIC_LOG_LEVEL=`heroku config:get NEW_RELIC_LOG_LEVEL --app $APPNAME`
+export REDIS_URL=`heroku config:get REDIS_URL --app $APPNAME`
+export STAGING_DATABASE_URL=`heroku config:get STAGING_DATABASE_URL --app $APPNAME`
+export CLOUDAMQP_APIKEY=`heroku config:get CLOUDAMQP_APIKEY --app $APPNAME`
+export CLOUDAMQP_URL=`heroku config:get CLOUDAMQP_URL --app $APPNAME`
+export PUSHER_SOCKET_URL=`heroku config:get PUSHER_SOCKET_URL --app $APPNAME`
+export PUSHER_URL=`heroku config:get PUSHER_URL --app $APPNAME`
 #oauth flow
 export APP_CLIENT_ID="3MVG9TSaZ8P6zP1oXkMT5WIwgzXQZXKdG560bpIt_.O7NGAwNDcr1b1WS_3EG7Hi9JBgSvcdcqr4E.N5gP8Yi"
 export APP_CLIENT_SECRET="youcantknow"
 # kafka part
-export KAFKA_CLIENT_CERT=`heroku config:get KAFKA_CLIENT_CERT  --app yourdemo`
-export KAFKA_CLIENT_CERT_KEY=`heroku config:get KAFKA_CLIENT_CERT_KEY  --app yourdemo`
-export KAFKA_PREFIX=`heroku config:get KAFKA_PREFIX  --app yourdemo`
-export KAFKA_TRUSTED_CERT=`heroku config:get KAFKA_TRUSTED_CERT  --app yourdemo`
-export KAFKA_URL=`heroku config:get KAFKA_URL  --app yourdemo`
+export KAFKA_CLIENT_CERT=`heroku config:get KAFKA_CLIENT_CERT  --app $APPNAME`
+export KAFKA_CLIENT_CERT_KEY=`heroku config:get KAFKA_CLIENT_CERT_KEY  --app $APPNAME`
+export KAFKA_PREFIX=`heroku config:get KAFKA_PREFIX  --app $APPNAME`
+export KAFKA_TRUSTED_CERT=`heroku config:get KAFKA_TRUSTED_CERT  --app $APPNAME`
+export KAFKA_URL=`heroku config:get KAFKA_URL  --app $APPNAME`
 export KAFKA_TOPIC_READ="topicRead"
 export KAFKA_TOPIC_WRITE="topicWrite"
 # logs
 export LOG_LEVEL=debug
 # blower$
-export BLOWERIO_URL=`heroku config:get BLOWERIO_URL  --app yourdemo`
+export BLOWERIO_URL=`heroku config:get BLOWERIO_URL  --app $APPNAME`
