7a757a68615b9b5ecd5f5b423bca6b1063e38b97
arieunier <arieunier@salesforce.com>
2019-02-12 17:39:47 +0100

    create script?

---
+++ createTables.sql
@@ -0,0 +1,15 @@
+drop table if exists public.badge;
+create table public.badge(id varchar(255) not null primary key, guest_id varchar(255) not null, guest_firstname varchar(255) not null, guest_lastname varchar(255) not null,
+        guest_company varchar(255), host_firstname varchar(255) not null, host_lastname varchar(255) not null, badge_status varchar(255), badge_url varchar(255), creation_date timestamp not null);
+drop table if exists public.packagingreviews;
+create table public.packagingreviews(
+            id varchar(255) not null primary key, 
+            ParticipantName varchar(255) not null, 
+            ParticipantEmail varchar(255) not null, 
+            BrandEvaluated varchar(255) not null,
+            GripReview varchar(255), 
+            PlugReview varchar(255) not null, 
+            PortabilityReview varchar(255) not null, 
+            Freetext varchar(255), 
+            image_url varchar(255), 
+            creation_date timestamp not null) ;
