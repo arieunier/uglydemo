7a1391b92ced2e64db946d009c6efc3be7abac5b
Augustin Rieunier <arieunier@arieunier-ltm2.internal.salesforce.com>
2019-04-26 11:31:02 +0200

    bye bye hugues

--- libs/blower.py
+++ libs/blower.py
@@ -2,15 +2,15 @@ import os
 import requests
 
 def sendMessage(Message, Phone):
-    #resp  = requests.post(os.environ['BLOWERIO_URL'] + '/messages', data={'to': Phone, 'message': Message})
-    #response = resp.json()
-    #print(resp.status_code)
-    #print(response)
-    hugue_url = "http://smssenddemo-knmf.us-e2.cloudhub.io/smssend"
-    resp  = requests.post(hugue_url, data={'numgsm': Phone, 'message': Message})
+    resp  = requests.post(os.environ['BLOWERIO_URL'] + '/messages', data={'to': Phone, 'message': Message})
     response = resp.json()
     print(resp.status_code)
     print(response)
+    #hugue_url = "http://smssenddemo-knmf.us-e2.cloudhub.io/smssend"
+    #resp  = requests.post(hugue_url, data={'numgsm': Phone, 'message': Message})
+    #response = resp.json()
+    #print(resp.status_code)
+    #print(response)
     
 
 #sendMessage("This is a test", "+33643395652")
